0fca3df6747dc22938e7b10ba06c26a67955ec01
shreya <abc@gmail.com>
2024-10-28 03:00:09 +0000

    updated age script

--- age.sh
+++ age.sh
@@ -7,6 +7,9 @@ echo "you are a child"
 elif [ $age -gt 13 -a $age -lt 19 ];
 then
 echo "you are a teenager"
+elif [ $age -gt 60 ];
+then
+echo "you are in oldage"
 else
 echo "you are an adult"
 fi
